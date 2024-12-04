fabc95771066100cac841f2e0cae512d0bf8480e
Naveen30134 <v.naveen5858@gmail.com>
2024-12-04 17:22:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,16 +3,16 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-count = 5
-ami = "ami-0ddc798b3f1a5117e"
+count = 4
+ami = "ami-0166fe664262f664c"
 instance_type = "t2.medium"
-key_name = "rahull"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+key_name = "babu"
+vpc_security_group_ids = ["sg-018100971f03ffabf"]
 tags = {
 Name = var.instance_names[count.index]
 }
 }
 
 variable "instance_names" {
-default = ["jenkins", "nexus", "tomcat-1", "tomcat-2", "Monitoring server"]
+default = ["jenkins", "nexus", "tomcat-1",  "Monitoring server"]
 }
