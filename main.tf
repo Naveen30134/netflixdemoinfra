8a30517213342d0b2eac746ee93169eb3c7cb856
Naveen30134 <v.naveen5858@gmail.com>
2025-02-14 12:35:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "us-east-1"
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-0166fe664262f664c"
+ami = "ami-053a45fff0a704a47"
 instance_type = "t2.medium"
 key_name = "babu"
 vpc_security_group_ids = ["sg-018100971f03ffabf"]
