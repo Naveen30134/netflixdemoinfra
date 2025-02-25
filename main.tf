17c64c13d6e4fe3cf9c46eacd3559c0217635a07
Naveen30134 <v.naveen5858@gmail.com>
2025-02-25 11:01:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-count = 4
+count = 5
 ami = "ami-04681163a08179f28"
 instance_type = "t2.medium"
 key_name = "babu"
@@ -14,5 +14,5 @@ Name = var.instance_names[count.index]
 }
 
 variable "instance_names" {
-default = ["jenkins", "nexus", "tomcat-1",  "Monitoring server"]
+default = ["jenkins", "nexus", "tomcat-1",  "Monitoring server","tomcat-2"]
 }
